0969854a06bdf3cc8e0d5c483cd6db5c77322d46
brandonc <45158177+beecars@users.noreply.github.com>
2026-04-14 21:46:31 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        else:
+            s_chars = {}
+            t_chars = {}
+            for char in s:
+                s_chars[char] = s_chars.get(char, 0) + 1
+            for char in t:
+                t_chars[char] = t_chars.get(char, 0) + 1
+
+        return s_chars == t_chars
